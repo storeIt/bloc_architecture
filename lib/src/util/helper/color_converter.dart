561d282e8d75e6eceeadf7a4410f608be3284c61
Petar Petrov <syncredex@gmail.com>
2023-03-28 16:13:00 +0300

    create: confirm dialog

---
+++ lib/src/util/helper/color_converter.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+class ColorConverter implements JsonConverter<Color, int> {
+  const ColorConverter();
+
+  @override
+  Color fromJson(int json) {
+    return Color(json);
+  }
+
+  @override
+  int toJson(Color object) {
+    return object.value;
+  }
+}
