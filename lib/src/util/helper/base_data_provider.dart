89282b15414974c771bdc09ffe92c7fae83e76ab
Petar Petrov <syncredex@gmail.com>
2023-03-28 22:42:51 +0300

    change: requests execution refactoring

--- lib/src/util/helper/base_data_provider.dart
+++ lib/src/util/helper/base_data_provider.dart
@@ -7,23 +7,17 @@ import 'package:dio/dio.dart';
 import '../exception/failure.dart';
 
 class BaseDataProvider {
-  final List<DioErrorType> connectionTimeoutErrorTypes = [
-    DioErrorType.connectionTimeout,
-    DioErrorType.receiveTimeout,
-    DioErrorType.sendTimeout,
-  ];
-
   Future<Either<Object, T>> executeRequest<T>({required Future request}) async {
     try {
       final response = await request;
       return Right(response);
-      // throw ServerFailure('Dummy error', DioError(requestOptions: RequestOptions()), StackTrace.current);
     } catch (error) {
-      return Left(error);
+      final failure = _onError(error, StackTrace.current);
+      return Left(failure);
     }
   }
 
-  Failure onError(Object error, StackTrace stackTrace) {
+  Failure _onError(Object error, StackTrace stackTrace) {
     switch (error.runtimeType) {
       case DioError:
         return ServerFailure(_mapDioErrorToMessage(error as DioError), error, StackTrace.current);
